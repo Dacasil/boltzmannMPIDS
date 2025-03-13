c5e42685bbcd3aa95ce17e3a053ea71e26a50efa
Mark Blümel <mark.bluemel@stud.uni-goettingen.de>
2025-03-13 15:20:07 +0100

    added statistics notebook

--- example_package/training.py
+++ example_package/training.py
@@ -5,14 +5,16 @@ from tqdm import tqdm
 
 
 def TrainBatch(model, data, epochs, learningRate,noise_levels,steps_statistics,
-               annealing_scheme:float|torch.Tensor,training_params=None,n_steps=None):
+               annealing_scheme:float|torch.Tensor,training_params=None,n_steps=None,tqdm=True):
         """
         annealing_scheme: temperature, or annealing scheme
         n_steps: number of steps to equilibrium, alternative to specifying a scheme 
         """
         optimizer = optim.SGD(model.parameters(), lr=learningRate)
-    
-        for iep in tqdm(range(epochs)):
+        iterator = range(epochs)
+        if tqdm:
+              iterator = tqdm(iterator)
+        for iep in iterator:
             model.training_step(optimizer,data,noise_levels,steps_statistics,annealing_scheme,n_steps,
                             **training_params )
             
