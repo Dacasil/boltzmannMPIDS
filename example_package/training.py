403ad7a61d488f119717a59cfe2c390b24d599ff
l.dacamarasilva <l.dacamarasilva@stud.uni-goettingen.de>
2025-05-07 13:39:09 +0200

    test_symmetry_statistics

--- example_package/training.py
+++ example_package/training.py
@@ -4,19 +4,33 @@ import matplotlib as plt
 from tqdm import tqdm
 
 
-def TrainBatch(model, data, epochs, learningRate,noise_levels,steps_statistics,
-               annealing_scheme:float|torch.Tensor,training_params=None,n_steps=None,tqdm=True):
-        """
-        annealing_scheme: temperature, or annealing scheme
-        n_steps: number of steps to equilibrium, alternative to specifying a scheme 
-        """
-        optimizer = optim.SGD(model.parameters(), lr=learningRate)
-        iterator = range(epochs)
-        if tqdm:
-              iterator = tqdm(iterator)
-        for iep in iterator:
-            model.training_step(optimizer,data,noise_levels,steps_statistics,annealing_scheme,n_steps,
-                            **training_params )
-            
-            
-
+def TrainBatch(
+    model,
+    data,
+    epochs,
+    learningRate,
+    noise_levels,
+    steps_statistics,
+    annealing_scheme: float | torch.Tensor,
+    training_params=None,
+    n_steps=None,
+    bar=True,
+):
+    """
+    annealing_scheme: temperature, or annealing scheme
+    n_steps: number of steps to equilibrium, alternative to specifying a scheme
+    """
+    optimizer = optim.SGD(model.parameters(), lr=learningRate)
+    iterator = range(epochs)
+    if bar:
+        iterator = tqdm(iterator)
+    for iep in iterator:
+        model.training_step(
+            optimizer,
+            data,
+            noise_levels,
+            steps_statistics,
+            annealing_scheme,
+            n_steps,
+            **training_params,
+        )
